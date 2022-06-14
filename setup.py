cfc596685b186be70230e12a6c00cd5a4a81ed0e
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2022-06-14 07:07:22 -0500

    published and fixed.

--- setup.py
+++ setup.py
@@ -1,25 +1,32 @@
-from distutils.core import setup
+from setuptools import setup
+import pathlib
+ 
+HERE = pathlib.Path(__file__).parent
+
+ 
+README = (HERE / "README.md").read_text()
+
 setup(
-  name = 'startapi',         # How you named your package folder (MyLib)
-  packages = ['startapi'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = ' A command line interface aplication to create boilerplate code for developing APIs',   # Give a short description about your library
-  author = 'teddyoweh',                   # Type in your name
-  author_email = 'teddy@teddyowehnet',      # Type in your E-Mail
-  url = 'https://github.com/teddy/startapi',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/teddy/startapi/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
-      ],
+  name = 'startapi',         
+  packages = ['startapi'],    
+  version = '0.1', 
+  description = 'A command line interface aplication to create boilerplate code for developing APIs',    
+  long_description=README,
+  long_description_content_type="text/markdown",     
+  license='MIT',       
+
+  author = 'teddyoweh',                    
+  author_email = 'teddy@teddyoweh.net',       
+  url = 'https://github.com/teddy/startapi',   
+  download_url = 'https://github.com/teddyoweh/startapi/archive/refs/tags/api.zip',     
+  keywords = ['startapi', 'FastAPI', 'FlaskAPI','API BOILERPLATE'],    
+ 
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 3 - Alpha',       
+    'Intended Audience :: Developers',      
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: MIT License',    
+    'Programming Language :: Python :: 3',       
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
